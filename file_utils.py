42c67cd1ce5e39503eeaa71d0651d935b0c73c92
Divyanshupanwar <divyanshupanwar32@gmail.com>
2025-05-15 20:45:01 +0530

    Add files via upload

---
+++ file_utils.py
@@ -0,0 +1,46 @@
+import io
+import base64
+import pandas as pd
+from pdfminer.layout import LAParams, LTTextBox
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+import streamlit as st
+
+def pdf_reader(file):
+    """
+    Extract text from PDF file
+    """
+    resource_manager = PDFResourceManager()
+    fake_file_handle = io.StringIO()
+    converter = TextConverter(resource_manager, fake_file_handle, laparams=LAParams())
+    page_interpreter = PDFPageInterpreter(resource_manager, converter)
+    with open(file, 'rb') as fh:
+        for page in PDFPage.get_pages(fh,
+                                    caching=True,
+                                    check_extractable=True):
+            page_interpreter.process_page(page)
+    text = fake_file_handle.getvalue()
+
+    # close open handles
+    converter.close()
+    fake_file_handle.close()
+    return text
+
+def show_pdf(file_path):
+    """
+    Display PDF in Streamlit
+    """
+    with open(file_path, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+    st.markdown(pdf_display, unsafe_allow_html=True)
+
+def get_table_download_link(df, filename, text):
+    """
+    Generates a link allowing the data in a given panda dataframe to be downloaded
+    """
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+    href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">{text}</a>'
+    return href
